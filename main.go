eeb370d5a1a451b2802dac52814c88a965c4f8cc
zlji <zlji@Ctrip.com>
2017-01-05 18:19:18 +0800

    cleanup main function

--- main.go
+++ main.go
@@ -12,43 +12,89 @@ import (
 	"github.com/yudai/gotty/backends"
 )
 
+var flags = []flag{
+	flag{"address", "a", "IP address to listen"},
+	flag{"port", "p", "Port number to listen"},
+	flag{"permit-write", "w", "Permit clients to write to the TTY (BE CAREFUL)"},
+	flag{"credential", "c", "Credential for Basic Authentication (ex: user:pass, default disabled)"},
+	flag{"random-url", "r", "Add a random string to the URL"},
+	flag{"random-url-length", "", "Random URL length"},
+	flag{"tls", "t", "Enable TLS/SSL"},
+	flag{"tls-crt", "", "TLS/SSL certificate file path"},
+	flag{"tls-key", "", "TLS/SSL key file path"},
+	flag{"tls-ca-crt", "", "TLS/SSL CA certificate file for client certifications"},
+	flag{"index", "", "Custom index.html file"},
+	flag{"title-format", "", "Title format of browser window"},
+	flag{"reconnect", "", "Enable reconnection"},
+	flag{"reconnect-time", "", "Time to reconnect"},
+	flag{"max-connection", "", "Maximum connection to gotty, 0(default) means no limit"},
+	flag{"once", "", "Accept only one client and exit on disconnection"},
+	flag{"permit-arguments", "", "Permit clients to send command line arguments in URL (e.g. http://example.com:8080/?arg=AAA&arg=BBB)"},
+	flag{"close-signal", "", "Signal sent to the command process when gotty close it (default: SIGHUP)"},
+}
+
+var mappingHint = map[string]string{
+	"index":      "IndexFile",
+	"tls":        "EnableTLS",
+	"tls-crt":    "TLSCrtFile",
+	"tls-key":    "TLSKeyFile",
+	"tls-ca-crt": "TLSCACrtFile",
+	"random-url": "EnableRandomUrl",
+	"reconnect":  "EnableReconnect",
+}
+
+func runApp(c *cli.Context) {
+	if len(c.Args()) == 0 {
+		msg := "Error: No command given."
+		cli.ShowAppHelp(c)
+		exit(fmt.Errorf(msg), 1)
+	}
+
+	options := app.DefaultOptions
+
+	configFile := c.String("config")
+	_, err := os.Stat(app.ExpandHomeDir(configFile))
+	if configFile != "~/.gotty" || !os.IsNotExist(err) {
+		if err := app.ApplyConfigFile(&options, configFile); err != nil {
+			exit(err, 2)
+		}
+	}
+
+	applyFlags(&options, flags, mappingHint, c)
+
+	if c.IsSet("credential") {
+		options.EnableBasicAuth = true
+	}
+	if c.IsSet("tls-ca-crt") {
+		options.EnableTLSClientAuth = true
+	}
+
+	if err := app.CheckConfig(&options); err != nil {
+		exit(err, 6)
+	}
+
+	manager := backends.NewCommandClientContextManager(c.Args(), options.CloseSignal)
+	app, err := app.New(c.Args(), manager, &options)
+	if err != nil {
+		exit(err, 3)
+	}
+
+	registerSignals(app)
+
+	err = app.Run()
+	if err != nil {
+		exit(err, 4)
+	}
+}
+
 func main() {
 	cmd := cli.NewApp()
-	cmd.Version = app.Version
 	cmd.Name = "gotty"
+	cmd.Version = app.Version
+	cmd.Action = runApp
 	cmd.Usage = "Share your terminal as a web application"
 	cmd.HideHelp = true
-
-	flags := []flag{
-		flag{"address", "a", "IP address to listen"},
-		flag{"port", "p", "Port number to listen"},
-		flag{"permit-write", "w", "Permit clients to write to the TTY (BE CAREFUL)"},
-		flag{"credential", "c", "Credential for Basic Authentication (ex: user:pass, default disabled)"},
-		flag{"random-url", "r", "Add a random string to the URL"},
-		flag{"random-url-length", "", "Random URL length"},
-		flag{"tls", "t", "Enable TLS/SSL"},
-		flag{"tls-crt", "", "TLS/SSL certificate file path"},
-		flag{"tls-key", "", "TLS/SSL key file path"},
-		flag{"tls-ca-crt", "", "TLS/SSL CA certificate file for client certifications"},
-		flag{"index", "", "Custom index.html file"},
-		flag{"title-format", "", "Title format of browser window"},
-		flag{"reconnect", "", "Enable reconnection"},
-		flag{"reconnect-time", "", "Time to reconnect"},
-		flag{"max-connection", "", "Maximum connection to gotty, 0(default) means no limit"},
-		flag{"once", "", "Accept only one client and exit on disconnection"},
-		flag{"permit-arguments", "", "Permit clients to send command line arguments in URL (e.g. http://example.com:8080/?arg=AAA&arg=BBB)"},
-		flag{"close-signal", "", "Signal sent to the command process when gotty close it (default: SIGHUP)"},
-	}
-
-	mappingHint := map[string]string{
-		"index":      "IndexFile",
-		"tls":        "EnableTLS",
-		"tls-crt":    "TLSCrtFile",
-		"tls-key":    "TLSKeyFile",
-		"tls-ca-crt": "TLSCACrtFile",
-		"random-url": "EnableRandomUrl",
-		"reconnect":  "EnableReconnect",
-	}
+	cli.AppHelpTemplate = helpTemplate
 
 	cliFlags, err := generateFlags(flags, mappingHint)
 	if err != nil {
@@ -64,52 +110,6 @@ func main() {
 			EnvVar: "GOTTY_CONFIG",
 		},
 	)
-
-	cmd.Action = func(c *cli.Context) {
-		if len(c.Args()) == 0 {
-			fmt.Println("Error: No command given.\n")
-			cli.ShowAppHelp(c)
-			exit(err, 1)
-		}
-
-		options := app.DefaultOptions
-
-		configFile := c.String("config")
-		_, err := os.Stat(app.ExpandHomeDir(configFile))
-		if configFile != "~/.gotty" || !os.IsNotExist(err) {
-			if err := app.ApplyConfigFile(&options, configFile); err != nil {
-				exit(err, 2)
-			}
-		}
-
-		applyFlags(&options, flags, mappingHint, c)
-
-		if c.IsSet("credential") {
-			options.EnableBasicAuth = true
-		}
-		if c.IsSet("tls-ca-crt") {
-			options.EnableTLSClientAuth = true
-		}
-
-		if err := app.CheckConfig(&options); err != nil {
-			exit(err, 6)
-		}
-
-		manager := backends.NewCommandClientContextManager(c.Args(), options.CloseSignal)
-		app, err := app.New(c.Args(), manager, &options)
-		if err != nil {
-			exit(err, 3)
-		}
-
-		registerSignals(app)
-
-		err = app.Run()
-		if err != nil {
-			exit(err, 4)
-		}
-	}
-
-	cli.AppHelpTemplate = helpTemplate
 
 	cmd.Run(os.Args)
 }
